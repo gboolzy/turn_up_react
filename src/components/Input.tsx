42e4bdee6f7bec9b75d3d05ff4141b4fbbf6abd2
user <giwagbolahan@live.com>
2025-09-21 01:43:40 +0100

    games

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -38,7 +38,7 @@ const Input: FC<InputProps> = ({
         <>
             <div className="mt-4 h-10 w-[100%] flex items-center border border-[rgba(255,255,255,0.08)] text-[#fff] px-[10px] rounded-[10px] gap-[10px] bg-[#0c1427]">
                 {icon && <i className={icon}></i>}
-                <input className={`w-[100%] h-[100%] outline-none border-none ${className || ""}`} type={type} {...register} name={name} id={id} placeholder={placeholder} value={value} onChange={onChange} />
+                <input className={`w-[100%] h-[100%] outline-none border-none pl-2 ${className || ""}`} type={type} {...register} name={name} id={id} placeholder={placeholder} value={value} onChange={onChange} />
             </div>
             {error && <p className="text-red-500 text-sm text-left ml-2 mt-1 font-[10px]">{error}</p>}
         </>
